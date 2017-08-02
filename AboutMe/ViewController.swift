537fb4c91aa72519a4b88de9af306cbe7fc079a6
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2017-08-02 14:46:29 -0500

    Initial commit

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        nameLabel.text = "Casey"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +28,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func moreInfo(_ sender: Any) {
+    }
 }
 
