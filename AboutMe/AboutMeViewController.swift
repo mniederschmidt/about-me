4626f4aeb7ebd779dc912c9168b203085c4578f9
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2017-11-05 22:47:18 -0600

    Made some changes to upgrade to Xcode 9 and added gesture recognition for pan and pinch on main picture

--- AboutMe/AboutMeViewController.swift
+++ AboutMe/AboutMeViewController.swift
@@ -41,6 +41,21 @@ class AboutMeViewController: UIViewController {
         super.viewDidLoad()
         nameLabel.text = "Casey"
     }
+    
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    @IBAction func handlePinch(recognizer: UIPinchGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+            recognizer.scale = 1
+        }
+    }
 
     @IBAction func moreInfo(_ sender: Any) {
     }
